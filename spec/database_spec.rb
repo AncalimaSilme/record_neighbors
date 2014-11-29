95ad4296653831f51687d8f1f8ece4da1fb7c631
Ancalima Silme <ancalimasilme@gmail.com>
2014-11-29 17:17:16 +0600

    added some tests

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,13 +2,14 @@ require_relative 'spec_helper.rb'
 require_relative 'support/database_config.rb'
 require_relative '../lib/rails_object_neighbors/base_extension.rb'
 
-
 RSpec.describe 'next()' do
     it "Должен вернуть следующий объект" do
       expect(Post.find(5)).to eql(Post.find(4).next)
     end
 
-    it "Должен вернуть nil если следующего элемента нет"
+    it "Должен вернуть nil если следующего элемента нет" do
+      expect(Post.find(10).next).to be_nil
+    end
 end
 
 RSpec.describe 'previous()' do
@@ -16,7 +17,9 @@ RSpec.describe 'previous()' do
       expect(Post.find(5)).to eql(Post.find(6).previous)
     end
 
-    it "Должен вернуть nil если предыдущего элемента нет"
+    it "Должен вернуть nil если предыдущего элемента нет" do
+      expect(Post.find(1).previous).to be_nil
+    end
 end
 
 RSpec.describe 'last?()' do
@@ -27,8 +30,6 @@ RSpec.describe 'last?()' do
     it "Должен вернуть ложь, если элемент не является последним" do
       expect(Post.find(5).last?).to be_falsey
     end
-
-    it "Должен выдать сообщение об ошибке, если элемент не существует"
 end
 
 RSpec.describe 'first?()' do
@@ -39,8 +40,6 @@ RSpec.describe 'first?()' do
     it "Должен вернуть ложь, если элемент не является первым" do
       expect(Post.find(5).first?).to be_falsey
     end
-
-    it "Должен выдать сообщение об ошибке, если элемент не существует"
 end
 
 RSpec.describe 'all_before()' do
@@ -56,11 +55,18 @@ RSpec.describe 'all_after()' do
 end
 
 RSpec.describe 'all_without()' do
-  it "Количество элементв должно быть model.length - 1"
-  it "Не должно быть элемента с ID 5"
+  it "Количество элементв должно быть model.length - 1" do
+    expect(Post.find(5).all_without.size).to eql(Post.all.size - 1)
+  end
+
+  it "В результате не должно быть объекта с ID 5" do
+    expect(Post.find(5).all_without).to_not include(Post.find(5))
+  end
 end
 
 
-RSpec.describe 'ActiveRecord::Base' do
-  it "Должен включать все методы плагина"
+RSpec.describe ActiveRecord::Base do
+  it "Должен включать все методы плагина" do
+    expect(ActiveRecord::Base.instance_methods).to include(:next, :previous, :first?, :last?, :all_before, :all_after, :all_without)
+  end
 end
